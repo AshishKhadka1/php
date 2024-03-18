4319575f79eb358f3227b93dc11f2665da78a472
Ashish khadka <khadkaashish50@gmail.com>
2024-03-18 23:26:05 +0545

    latest

--- broCode/session/index.php
+++ broCode/session/index.php
@@ -1,24 +1,45 @@
 <?php
-  session_start();
+session_start();
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Login</title>
 </head>
+
 <body>
-   <form action="form.php" method="post">
-     <label for="">username: </label>
-    <input type="text" name="username" id=""><br><br>
+  <form action="index.php" method="post">
+    <label for="username">Username:</label>
+    <input type="text" name="username" id="username"><br><br>
 
-    <label for="">password: </label>
-    <input type="text" name="password" id=""><br><br>
+    <label for="password">Password:</label>
+    <input type="password" name="password" id="password"><br><br>
 
-    <input type="submit" value="submit" id="">
-   </form>
+    <input type="submit" name="login" value="Login">
+  </form>
 
 </body>
+
 </html>
+
+<?php
+if (isset($_POST["login"])) {
+
+  if (
+    !empty($_POST["username"]) &&
+    !empty($_POST["password"])
+  ) {
+
+    $_SESSION["username"] = $_POST["username"];
+    $_SESSION["password"] = $_POST["password"];
+
+    header("location: home.php");
+  } else {
+    echo "Missing username/Password<br>";
+  }
+}
+?>
